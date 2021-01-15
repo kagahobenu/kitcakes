4084bf74df4f920e60ab2a6cfd294af508ebfbc5
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-U2SA7DV>
2021-01-15 09:57:00 -0300

    Addd da pagina contatos, 15/01/2021

---
+++ paginas/kitcakescontato.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contato</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css.css" />
+        <link rel="stylesheet" href="plugins/icomoon2/icomoon.css.css" />
+        <link rel="stylesheet" href="assets/css/kitcakes.css"/>
+    </head>
+    <body>
+        <div class="container mt-5">
+            <h1 class="text-center pb-2">Contato</h1>
+            <div class="form w-75 h-50 mx-auto ">
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        
+                        <div class="form-group">
+                            <span class="vic-user"></span>  
+                            <label for="exampleFormControlInput1">Nome completo</label>                           
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <span class="vic-whatsapp"></span>  
+                            <label for="exampleFormControlInput1">Celular</label>
+
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <span class="vic-mail"></span>
+                            <label for="exampleFormControlInput1">E-mail</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <span class="vic-news-paper"></span>
+                            <label for="exampleFormControlInput1">CPF</label>
+                            <span class="manu-document"></span>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div> 
+                        <div class="form-group">
+                            <span class="vic-qr_code"></span>
+                            <label for="exampleFormControlInput1">Código do produto</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <span class="vic-cake"></span>
+                            <label for="exampleFormControlSelect1">Sabor da massa</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Chocolate</option>
+                                <option>baunilha</option> 
+                            </select>
+                        </div>       
+                        <div class="form-group">
+                            <span class="vic-cake"></span>
+                            <label for="exampleFormControlSelect1">Sabor do recheio</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Chocolate</option>
+                                <option>Leite ninho</option> 
+                                <option>Maracujá</option> 
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <span class="vic-cake"></span>
+                            <label for="exampleFormControlSelect1">Nossas opções de kit</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>kit 1</option>
+                                <option>Kit 2</option> 
+                                <option>Kit 3</option> 
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <span class="vic-cake"></span>
+                            <label for="exampleFormControlSelect1">Peso da torta</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>1 kg</option>
+                                <option>2 kg</option>
+                                <option>3 kg</option>
+
+                            </select>
+                        </div> 
+                        <div class="form-group">
+                            <span class="vic-credit_card"></span>
+                            <label for="exampleFormControlSelect1">Forma de pagamento</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Crédito</option>
+                                <option>Débito</option>
+                                <option>Boleto</option>
+                                <option>Outros</option>
+                            </select>
+                        </div> 
+                    </div>
+                </div>
+                <div class="form-group w-100 ">
+                    <span class="vic-chat_bubble_outline"></span>
+                    <label for="exampleFormControlTextarea1">Observação</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button class="btn btn-block btn-danger">Enviar Pedido</button>
+            </div>
+        </div>
+    </body>
+</html>
